82b1543a9739e61b3f9d97af445cd98941b90ed1
yyang_1207@163.com <yyang_1207@163.com>
2020-02-24 14:53:01 +0800

    动态路由

--- APIGateway/Startup.cs
+++ APIGateway/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace APIGateway
 {
@@ -27,7 +28,7 @@ namespace APIGateway
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddControllers();
-            services.AddOcelot();
+            services.AddOcelot().AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
